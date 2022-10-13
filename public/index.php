b1425560fab1dccb2bd01debafc51a7994afa1b1
Andreas Mischke <andreas@mischke.me>
2022-10-13 16:18:28 +0200

    feat: setup Config, phpdotenv and Slim error middleware

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 
+use PonyForm\Config\Config;
 use PonyForm\Controller\HomepageController;
 use Slim\Factory\AppFactory;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$BASEDIR = dirname(__DIR__);
 
+require_once $BASEDIR . '/vendor/autoload.php';
+
+$cfg = new Config($BASEDIR);
 $app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware($cfg->DEBUG, true, true);
 
 $app->get('/', new HomepageController());
 
